85514000d10dd303919010395d450f18b488bec8
Travis Gerke <tgerke@mail.harvard.edu>
2025-09-26 13:47:25 -0700

    make checks happy

--- R/attach_ducklake.R
+++ R/attach_ducklake.R
@@ -4,7 +4,7 @@
 #' It will create a new DuckDB-backed DuckLake if the specified name does not exist, or connect to the existing DuckLake if it does exist.
 #'
 #' @param ducklake_name Name for the ducklake file, as in `ducklake:{ducklake_name}.ducklake`
-#' @param data_path Optional directory where Parquet files are stored. If not specified, uses the default folder {ducklake_name}.ducklake.files in the same directory as the DuckLake itself.
+#' @param data_path Optional directory where Parquet files are stored. If not specified, uses the default folder `{ducklake_name}.ducklake.files` in the same directory as the DuckLake itself.
 #'
 #' @returns NULL
 #' @export
